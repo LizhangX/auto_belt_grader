45bfc027996da05512941fe699cae767c3d8d080
Lizhang_Xie <lizhang.xie@gmail.com>
2017-10-17 20:27:11 -0500

    added html and css validator function for the app

--- apps/auto_belt_grader/grader.py
+++ apps/auto_belt_grader/grader.py
@@ -20,19 +20,58 @@ def analyze(shortName, upload_id):
     options.add_argument('headless')
     options.add_argument('window-size=1200x1800')
     driver = webdriver.Chrome(chrome_options=options)
+    messages = []
 
     #using PhantomJS
     # driver = webdriver.PhantomJS()
     # driver.set_window_size(1620, 1080)
     # driver.get('https://google.com')
-    print "making screenshot"
-    file =  glob.glob('media/documents/{}/{}/*.html'.format(upload_id, shortName))
+
+    ### getting html files path
+    file = glob.glob('media/documents/{}/{}/*.html'.format(upload_id, shortName))
+    cssFile = glob.glob('media/documents/{}/{}/*.css'.format(upload_id, shortName))
+
     if file[0]:
         filePath = 'file://' + os.getcwd() + "/" +file[0]
-        print filePath
         driver.get(filePath)
+        print "making screenshot of the html"
         driver.save_screenshot('media/documents/{}/screen.png'.format(upload_id))
+        
+        ### - count the number of unique html errors: http://validator.w3.org/
+        driver.get('https://validator.w3.org/#validate_by_upload')
+        driver.find_element_by_id('uploaded_file').send_keys(os.getcwd() + "/" +file[0])
+        element = driver.find_element_by_css_selector('#validate-by-upload form')
+        element.submit()
+        # driver.implicitly_wait(2)
+        elements = driver.find_elements_by_css_selector('li.error')
+        if len(elements) == 0:
+            # success
+            messages.append("No errors on HTML validator.")
+        else:
+            # failed
+            # len(elements) number of errors in html validator
+            messages.append("Has {} error(s) on HTML validator.".format(len(elements)))
+        
+    if cssFile[0]:
+        ### - count the number of unique css errors: https://jigsaw.w3.org/css-validator/
+        driver.get('https://jigsaw.w3.org/css-validator/#validate_by_upload')
+        driver.find_element_by_id('file').send_keys(os.getcwd() + "/" + cssFile[0])
+        element = driver.find_element_by_css_selector('#validate-by-upload form')
+        element.submit()
+
+        elements = driver.find_elements_by_css_selector('tr.error')
+        if len(elements) == 0:
+            # success
+            messages.append("No errors on CSS validator.")
+        else:
+            # failed
+            # len(elements) number of errors in html validator
+            messages.append("Has {} error(s) on CSS validator.".format(len(elements)))
+        
+        
+    driver.save_screenshot('media/documents/{}/test.png'.format(upload_id)) 
     driver.quit()
+    return messages
 
 
 
--- apps/auto_belt_grader/views.py
+++ apps/auto_belt_grader/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import User
 from .models import Belt
 import os
 from . import grader
 import shutil
+
 # Create your views here.
 def index(request):
-    if 'errors' not in request.session:
-        request.session['errors'] = []
     return render(request,'login_registration_app/index.html')
 
 def success(request):
@@ -29,7 +29,9 @@ def reg(request):
     check = User.objects.register(request.POST)
     if check:
         print type(check)
-        request.session['errors'] = check
+        for i in check:
+            messages.add_message(request, messages.INFO, i)
+        # request.session['errors'] = check
         return redirect('/')
 
     user = User.objects.create(
@@ -38,7 +40,7 @@ def reg(request):
         password=request.POST['password']
     )
     request.session['user_id'] = user.id
-    request.session['errors'] = []
+    # request.session['errors'] = []
     request.session['status'] = 'registerted'
     return redirect('/success')
 
@@ -46,12 +48,14 @@ def login(request):
     log = User.objects.login(request.POST)
     if type(log) == type(User()):
         request.session['user_id'] = log.id
-        request.session['errors'] = []   
+        # request.session['errors'] = []   
         request.session['status'] = 'logged in'
         return redirect('/success')
     else:
         print log
-        request.session['errors'] = log
+        for i in log:
+            messages.add_message(request, messages.INFO, i)
+        # request.session['errors'] = log
     return redirect('/')
 
 def upload(request):
@@ -79,7 +83,13 @@ def analyze(request, upload_id):
     shortName = name[10:name.find('.')]
     # print "shortName", shortName
     grader.unzip(name, upload_id)
-    grader.analyze(shortName, upload_id)
+    # flash messages
+    msgs = grader.analyze(shortName, upload_id)
+    messages.add_message(request, messages.INFO, "Scanned folder <<" + shortName + ">> : ")
+    for message in msgs:
+        messages.add_message(request, messages.INFO, message)
 
+    
+  
     return redirect('/success')
     
